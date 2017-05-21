01b6f52e4d0d20fd032abb7ccc5144d4707aa1ea
m-herrera <m.herrera0799@gmail.com>
2017-05-21 15:40:40 -0600

    Efecto de aceleracion

--- Robot Virtual 2.py
+++ Robot Virtual 2.py
@@ -7,76 +7,97 @@ import pygame
 import time
 import os 
 
+pygame.mixer.init()
+
+def cargarImagen(nombre):
+    ruta = os.path.join("Robot Sprite", nombre)
+    imagen = pygame.image.load(ruta)
+    return imagen
+
+def cargarSonido(nombre):
+    ruta = os.path.join("Audio", nombre)
+    sonido = pygame.mixer.Sound(ruta)
+    return sonido
+
 class Robot:
 
     def __init__(self):
         self.Nombre = "Nombre"
-        self.sprite = pygame.image.load("Robot Sprite\Idle (1).png")
-        self.posx = 200
-        self.posy = 200
-
-    def getNombre (self):
+        self.sprite = cargarImagen("Idle (1).png")
+        self.posx = 300
+        self.posy = 300
+        self.x_velocidad = 0
+        self.y_velocidad = 0
+        self.aceleracion = 3
+    def get_nombre (self):
         return self.Nombre
+    
     def get_posx(self):
         return self.posx
     def get_posy(self):
         return self.posy
-    def mov_posx(self, pos):
+    
+    def set_posx(self, pos):
         self.posx += pos
-    def mov_posy(self, pos):
+    def set_posy(self, pos):
         self.posy += pos
 
+
+    def acelerar_x(self,direccion): #direccion 1 acelera der , direccion -1 acelera izq
+        self.x_velocidad += direccion*self.aceleracion
+        self.set_posx(self.x_velocidad) 
+    def go_left(self):
+        pass
+    
 def scale_img(image,width,height):
     image = pygame.transform.smoothscale(image,(width,height))
     return image
 
-clock = pygame.time.Clock()
-x_speed = 0
-y_speed = 0
 
-paco = Robot()
-paco.sprite = scale_img(paco.sprite,200,200)
+clock = pygame.time.Clock()
 
 pygame.init()
 
 ventana = pygame.display.set_mode((800,600))
-pygame.display.set_caption(paco.getNombre())
+pygame.display.set_caption("Nombre ventana")
 
 
-inGame = True
 
-while inGame :
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT:
-            inGame = False
+def inGame (robot):
+    in_Game = True
+    while in_Game :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                in_Game = False
 
-        if event.type == pygame.KEYDOWN :
-            if event.key == pygame.K_LEFT:
-                x_speed = -5
-                y_speed = 0
-            elif event.key == pygame.K_RIGHT:
-                x_speed = 5
-                y_speed = 0
-            elif event.key == pygame.K_UP:
-                y_speed = -5
-                x_speed = 0
-            elif event.key == pygame.K_DOWN:
-                y_speed = 5
-                x_speed = 0
+            if event.type == pygame.KEYDOWN :
+                if event.key == pygame.K_LEFT:
+                    robot.acelerar_x(-1)
+                elif event.key == pygame.K_RIGHT:
+                    robot.acelerar_x(1)
+                pygame.event.clear()
+                pygame.event.post(event)
                 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_speed = 0
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                y_speed = 0
-            
-    paco.mov_posx(x_speed)
-    paco.mov_posy(y_speed)
-    ventana.fill((255,255,255))
-    ventana.blit(paco.sprite,(paco.posx,paco.posy))
-    pygame.display.update()
-    clock.tick(30)
+            elif event.type == pygame.KEYUP:
+                if robot.x_velocidad == 0:
+                    pygame.event.clear()
+                elif event.key == pygame.K_LEFT:
+                    robot.acelerar_x(1)
+                elif  event.key == pygame.K_RIGHT:
+                    robot.acelerar_x(-1)
+                pygame.event.clear()
+                pygame.event.post(event)
+        
+        ventana.fill((255,255,255))
+        ventana.blit(robot.sprite,(robot.posx,robot.posy))
+        pygame.display.update()
+        clock.tick(30)
 
 
-pygame.quit()
-quit()
+    pygame.quit()
+    quit()
+
+paco = Robot()
+paco.sprite = scale_img(paco.sprite,200,200)
+
+inGame(paco)
