3294e86573df129556370e1f0da842f5d19e56ad
JassonRM <jassonrm@icloud.com>
2017-05-27 20:36:52 -0600

    Control con botones

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -1,8 +1,16 @@
 #define ejeX A0
 #define ejeY A1
+#define Boton1 2
+#define Boton2 3
+#define Boton3 4
+#define Boton4 5
 
-String DataEjeX = "";
-String DataEjeY = "";
+String DataEjeX;
+String DataEjeY;
+String A;
+String B;
+String C;
+String D;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,7 +24,12 @@ void loop() {
   
 DataEjeX = analogRead(ejeX);
 DataEjeY = analogRead(ejeY);
-String Data = "{'X' : " + DataEjeX + ", 'Y' : " + DataEjeY + "}";
+A = digitalRead(Boton1);
+B = digitalRead(Boton2);
+C = digitalRead(Boton3);
+D = digitalRead(Boton4);
+
+String Data = "{'X' : " + DataEjeX + ", 'Y' : " + DataEjeY + ", 'A ': " + A + ", 'B' : " + B + ", 'C' : " + C + ", 'D' : " + D + "}";
 
 Serial.println(Data);
 
