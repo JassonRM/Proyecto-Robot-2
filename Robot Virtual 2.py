2f577f7f94aaf7fe8cb460220f715bc45726ff62
m-herrera <m.herrera0799@gmail.com>
2017-06-01 22:54:34 -0600

    algunos cambios

--- Robot Virtual 2.py
+++ Robot Virtual 2.py
@@ -17,9 +17,21 @@ from threading import Thread
 #Puerto Bluetooth
 puerto = 'com3'
 
+#objeto serial
 arduino = serial.Serial(puerto, 9600, timeout=None)
 arduino.flushInput()
 
+#Tamaño de sprites 
+spriteSize = 200
+bulletSize = 20
+
+# Variables globales
+right = True
+inbullet = False
+sliding = False
+bala = None
+
+#------------------------------------------------------------------------Funciones------------------------------------------------------------------------#
 
 #Funcion: serialCom
 #Entrada: Dispositivo
@@ -49,8 +61,17 @@ def cargarImagen(nombre):
 #Restricciones: nombre es un string
 def cargarSonido(nombre):
     ruta = os.path.join("Audio", nombre)
-    sonido = pygame.mixer.music.load(ruta)
+    sonido = pygame.mixer.Sound(ruta)
     return sonido
+
+#Funcion: cargarMusica
+#Entrada: nombre
+#Salida: musica con ese nombre en el directorio audio
+#Restricciones: nombre es un string
+def cargarMusica(nombre):
+    ruta = os.path.join("Audio", nombre)
+    musica = pygame.mixer.music.load(ruta)
+    return musica
 
 #Funcion: scale_img
 #Entradas: imagen, ancho y altura
@@ -60,16 +81,33 @@ def scale_img(image,width,height):
     image = pygame.transform.smoothscale(image,(width,height))
     return image
 
-spriteSize = 200
-bulletSize = 20
-Sprite = {"idle1":scale_img(cargarImagen("Idle (1).png"),spriteSize,spriteSize), "run1":scale_img(cargarImagen("Run (1).png"),spriteSize,spriteSize), "run2":scale_img(cargarImagen("Run (2).png"),spriteSize,spriteSize), "run3":scale_img(cargarImagen("Run (3).png"),spriteSize,spriteSize), "run4":scale_img(cargarImagen("Run (4).png"),spriteSize,spriteSize), "run5":scale_img(cargarImagen("Run (5).png"),spriteSize,spriteSize), "run6":scale_img(cargarImagen("Run (6).png"),spriteSize,spriteSize), "run7":scale_img(cargarImagen("Run (7).png"),spriteSize,spriteSize), "run8":scale_img(cargarImagen("Run (8).png"),spriteSize,spriteSize), "jump1":scale_img(cargarImagen("Jump (1).png"),spriteSize,spriteSize), "jump2":scale_img(cargarImagen("Jump (2).png"),spriteSize,spriteSize), "jump3":scale_img(cargarImagen("Jump (3).png"),spriteSize,spriteSize), "jump4":scale_img(cargarImagen("Jump (4).png"),spriteSize,spriteSize), "jump5":scale_img(cargarImagen("Jump (5).png"),spriteSize,spriteSize), "jump6":scale_img(cargarImagen("Jump (6).png"),spriteSize,spriteSize), "jump7":scale_img(cargarImagen("Jump (7).png"),spriteSize,spriteSize), "jump8":scale_img(cargarImagen("Jump (8).png"),spriteSize,spriteSize), "jump9":scale_img(cargarImagen("Jump (9).png"),spriteSize,spriteSize), "jump10":scale_img(cargarImagen("Jump (10).png"),spriteSize,spriteSize)
-          , "run_s1":scale_img(cargarImagen("RunShoot (1).png"),spriteSize,spriteSize), "run_s2":scale_img(cargarImagen("RunShoot (2).png"),spriteSize,spriteSize),"run_s3":scale_img(cargarImagen("RunShoot (3).png"),spriteSize,spriteSize), "run_s4":scale_img(cargarImagen("RunShoot (4).png"),spriteSize,spriteSize), "run_s5":scale_img(cargarImagen("RunShoot (5).png"),spriteSize,spriteSize), "run_s6":scale_img(cargarImagen("RunShoot (6).png"),spriteSize,spriteSize), "run_s7":scale_img(cargarImagen("RunShoot (7).png"),spriteSize,spriteSize), "run_s8":scale_img(cargarImagen("RunShoot (8).png"),spriteSize,spriteSize), "shoot1":scale_img(cargarImagen("Shoot (1).png"),spriteSize,spriteSize), "bullet1":scale_img(cargarImagen("Bullet_001.png"),bulletSize,bulletSize),"slide1":scale_img(cargarImagen("Slide (1).png"),spriteSize,spriteSize),"slide2":scale_img(cargarImagen("Slide (2).png"),spriteSize,spriteSize),"slide3":scale_img(cargarImagen("Slide (3).png"),spriteSize,spriteSize),"slide4":scale_img(cargarImagen("Slide (4).png"),spriteSize,spriteSize),"slide5":scale_img(cargarImagen("Slide (5).png"),spriteSize,spriteSize),"slide6":scale_img(cargarImagen("Slide (6).png"),spriteSize,spriteSize),"slide7":scale_img(cargarImagen("Slide (7).png"),spriteSize,spriteSize),"slide8":scale_img(cargarImagen("Slide (8).png"),spriteSize,spriteSize)}
+#Diccionario con las imagenes del robot
+Sprite = {"idle1":scale_img(cargarImagen("Idle (1).png"),spriteSize,spriteSize),"idle2":scale_img(cargarImagen("Idle (2).png"),spriteSize,spriteSize),
+          "idle3":scale_img(cargarImagen("Idle (3).png"),spriteSize,spriteSize),"idle4":scale_img(cargarImagen("Idle (4).png"),spriteSize,spriteSize),
+          "idle4":scale_img(cargarImagen("Idle (4).png"),spriteSize,spriteSize),"idle5":scale_img(cargarImagen("Idle (5).png"),spriteSize,spriteSize),
+          "idle6":scale_img(cargarImagen("Idle (6).png"),spriteSize,spriteSize),"idle7":scale_img(cargarImagen("Idle (7).png"),spriteSize,spriteSize),
+          "idle8":scale_img(cargarImagen("Idle (8).png"),spriteSize,spriteSize),"idle9":scale_img(cargarImagen("Idle (9).png"),spriteSize,spriteSize),
+          "idle10":scale_img(cargarImagen("Idle (10).png"),spriteSize,spriteSize),"run1":scale_img(cargarImagen("Run (1).png"),spriteSize,spriteSize),
+          "run2":scale_img(cargarImagen("Run (2).png"),spriteSize,spriteSize), "run3":scale_img(cargarImagen("Run (3).png"),spriteSize,spriteSize),
+          "run4":scale_img(cargarImagen("Run (4).png"),spriteSize,spriteSize), "run5":scale_img(cargarImagen("Run (5).png"),spriteSize,spriteSize),
+          "run6":scale_img(cargarImagen("Run (6).png"),spriteSize,spriteSize), "run7":scale_img(cargarImagen("Run (7).png"),spriteSize,spriteSize),
+          "run8":scale_img(cargarImagen("Run (8).png"),spriteSize,spriteSize), "jump1":scale_img(cargarImagen("Jump (1).png"),spriteSize,spriteSize),
+          "jump2":scale_img(cargarImagen("Jump (2).png"),spriteSize,spriteSize), "jump3":scale_img(cargarImagen("Jump (3).png"),spriteSize,spriteSize),
+          "jump4":scale_img(cargarImagen("Jump (4).png"),spriteSize,spriteSize), "jump5":scale_img(cargarImagen("Jump (5).png"),spriteSize,spriteSize),
+          "jump6":scale_img(cargarImagen("Jump (6).png"),spriteSize,spriteSize), "jump7":scale_img(cargarImagen("Jump (7).png"),spriteSize,spriteSize),
+          "jump8":scale_img(cargarImagen("Jump (8).png"),spriteSize,spriteSize), "jump9":scale_img(cargarImagen("Jump (9).png"),spriteSize,spriteSize),
+          "jump10":scale_img(cargarImagen("Jump (10).png"),spriteSize,spriteSize), "run_s1":scale_img(cargarImagen("RunShoot (1).png"),spriteSize,spriteSize),
+          "run_s2":scale_img(cargarImagen("RunShoot (2).png"),spriteSize,spriteSize),"run_s3":scale_img(cargarImagen("RunShoot (3).png"),spriteSize,spriteSize),
+          "run_s4":scale_img(cargarImagen("RunShoot (4).png"),spriteSize,spriteSize), "run_s5":scale_img(cargarImagen("RunShoot (5).png"),spriteSize,spriteSize),
+          "run_s6":scale_img(cargarImagen("RunShoot (6).png"),spriteSize,spriteSize), "run_s7":scale_img(cargarImagen("RunShoot (7).png"),spriteSize,spriteSize),
+          "run_s8":scale_img(cargarImagen("RunShoot (8).png"),spriteSize,spriteSize), "shoot1":scale_img(cargarImagen("Shoot (1).png"),spriteSize,spriteSize),
+          "bullet1":scale_img(cargarImagen("Bullet_001.png"),bulletSize,bulletSize),"slide1":scale_img(cargarImagen("Slide (1).png"),spriteSize,spriteSize),
+          "slide2":scale_img(cargarImagen("Slide (2).png"),spriteSize,spriteSize),"slide3":scale_img(cargarImagen("Slide (3).png"),spriteSize,spriteSize),
+          "slide4":scale_img(cargarImagen("Slide (4).png"),spriteSize,spriteSize),"slide5":scale_img(cargarImagen("Slide (5).png"),spriteSize,spriteSize),
+          "slide6":scale_img(cargarImagen("Slide (6).png"),spriteSize,spriteSize),"slide7":scale_img(cargarImagen("Slide (7).png"),spriteSize,spriteSize),
+          "slide8":scale_img(cargarImagen("Slide (8).png"),spriteSize,spriteSize)}
 
-# Variables globales
-right = True
-inbullet = False
-sliding = False
-bala = None
+#------------------------------------------------------------------------Clases------------------------------------------------------------------------#
 
 class Robot:
     def __init__(self):
@@ -162,6 +200,22 @@ class Robot:
         else:
             self.cambiar_sprite_izquierda("shoot1")
 
+    #Metodo: shoot
+    #Entrada: ninguna
+    #Salida: cambia el sprite en modo standing
+    #Restricciones:
+    def stand(self):
+        if self.imagen >= 8:
+            self.imagen = 1
+        else:
+            self.imagen += 0.5
+        if right:
+            self.cambiar_sprite_derecha("idle"+ str(int((self.imagen))))
+        else:
+            self.cambiar_sprite_izquierda("idle"+ str(int(self.imagen)))
+    
+        
+
     #Metodo: jump
     #Entrada: ninguna
     #Salida: cambia la posicion en y y el sprite para que salte
@@ -225,7 +279,11 @@ class Bullet:
             self.posy = robot.get_posy() + 100
             self.velocidad = -20
             self.imagen = pygame.transform.flip(Sprite["bullet1"],True,False)
-
+            
+    #Metodo: shoot
+    #Entrada: ninguna
+    #Salida: cambia la posicion en x de la bala
+    #Restricciones: ninguna
     def shoot(self):
         global inbullet
         if self.posx > 0 and self.posx < 1000:
@@ -233,14 +291,19 @@ class Bullet:
         else:
             inbullet = False
 
+#------------------------------------------------------------------------Interfaz------------------------------------------------------------------------#
+
 #Inicializar pygame
 pygame.init()
 clock = pygame.time.Clock()
 
-#Inicializar el mixer de pygame
+#Inicializar el mixer de pygame y cargar audios 
 pygame.mixer.init(frequency=8000)
 cancion = "Epic Orchestral Action.wav"
-cargarSonido(cancion)
+cargarMusica(cancion)
+mensaje = "Presentación.wav"
+presentacion = cargarSonido(mensaje)
+
 
 #Crear ventana
 windowWidth = 1000
@@ -272,7 +335,9 @@ def inGame (robot):
         if inbullet:
             ventana.blit(bala.imagen, (bala.posx,bala.posy))
         pygame.display.flip()
-        clock.tick(30)
+        clock.tick(60)
+
+#-------------------------------------------------------------------Analisis de Datos Recibidos-------------------------------------------------------------------#
 
 #Funcion: controller
 #Entrada: instancia del robot a controlar
@@ -295,7 +360,7 @@ def controller(robot):
             # Right
             if keys["X"] > 561 and robot.get_posx() < windowWidth - 125:
                 # Slide
-                if keys["Y"] > 750 and robot.tiempo == 0 and keys["X"] > 950:
+                if keys["Y"] > 600 and robot.tiempo == 0 and keys["X"] > 950:
                     robot.slide(keys["X"],keys)
                 # Run-Shoot
                 elif keys["B"] == 1 and robot.tiempo == 0:
@@ -309,7 +374,7 @@ def controller(robot):
             # Left
             elif keys["X"] < 461 and robot.get_posx() > -75:
                 # Slide
-                if keys["Y"] > 750 and robot.tiempo == 0 and keys["X"] < 50:
+                if keys["Y"] > 600 and robot.tiempo == 0 and keys["X"] < 50:
                     robot.slide(keys["X"],keys)
                 # Run-Shoot
                 elif keys["B"] == 1 and robot.tiempo == 0 and not inbullet:
@@ -320,16 +385,15 @@ def controller(robot):
                 # Run
                 else:
                     robot.run(keys["X"])
-        
+            #Shoot
             elif keys["B"] == 1 and robot.tiempo == 0 and not inbullet:
                 inbullet = True
                 bala = Bullet(robot)
                 robot.shoot()
+            #Stand
+            elif robot.tiempo == 0:
+                robot.stand()
 
-            elif right and robot.tiempo == 0:
-                robot.cambiar_sprite_derecha("idle1")
-            elif not right and robot.tiempo == 0:
-                robot.cambiar_sprite_izquierda("idle1")
             disparo_anterior = keys["B"]
             # Music
             if keys["A"] == 0 and musica_anterior == 1 and not music_on:
@@ -341,7 +405,10 @@ def controller(robot):
             elif keys["A"] == 0 and musica_anterior == 1 :
                 pygame.mixer.music.pause()
                 music_on = False
-            
+            #Presentacion
+            if keys["D"] == 1 and not music_on:
+                presentacion.play()            
+                
             musica_anterior = keys["A"]
             
             
